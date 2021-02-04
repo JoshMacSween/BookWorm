f1ab449f34cf5ac774ce15f62b482dc76a67360c
Joshua MacSween <joshmacsween@gmail.com>
2021-02-04 16:35:51 -0700

    navigate by card press

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,11 +1,11 @@
-import {faBook} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Button} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {Card} from 'react-native-elements';
 
 export default function BookCard({book, bookId, navigation}) {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Details', {bookId: bookId})}>
       <Card>
         {book && book.title ? (
           <View>
@@ -14,13 +14,8 @@ export default function BookCard({book, bookId, navigation}) {
             <Text style={styles.subtitle}>{book.authors}</Text>
           </View>
         ) : (
-          // {book.authors}
           <Text style={styles.title}>No books yet!</Text>
         )}
-        <Button
-          title="Details"
-          onPress={() => navigation.navigate('Details', {bookId: bookId})}
-        />
       </Card>
     </TouchableOpacity>
   );
