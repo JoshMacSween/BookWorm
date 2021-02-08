827a6a9103caf82195bf18d1eb1b0518f8395cee
Joshua MacSween <joshmacsween@gmail.com>
2021-02-07 20:32:46 -0700

    undefined image bug fix

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,17 +10,6 @@ import axios from 'axios';
 
 export default function HomeScreen({navigation}) {
   const {fetchBooks, books} = useContext(BooksContext);
-  // const [books, setBooks] = useState([]);
-
-  // async function fetchBooks(q) {
-  //   const result = await axios
-  //     .get(
-  //       `https://www.googleapis.com/books/v1/volumes?q=${q}&key=${GOOGLE_API_KEY}`,
-  //     )
-  //     .then((response) => {
-  //       setBooks(response.data.items);
-  //     });
-  // }
 
   return (
     <View style={styles.center}>
@@ -33,7 +22,10 @@ export default function HomeScreen({navigation}) {
             key={item.id}
             bookId={item.id}
             book={item.volumeInfo}
-            thumbnail={item.volumeInfo.imageLinks.smallThumbnail}
+            thumbnail={
+              item.volumeInfo.imageLinks &&
+              item.volumeInfo.imageLinks.smallThumbnail
+            }
             navigation={navigation}
           />
         )}
@@ -49,21 +41,3 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
 });
-
-// const [count, setCount] = React.useState(0);
-// React.useLayoutEffect(() => {
-//   navigation.setOptions({
-//     headerRight: () => (
-//       <Button onPress={() => setCount((c) => c + 1)} title="Update Count" />
-//     ),
-//   });
-// }, [navigation]);
-// return (
-//   <Button
-//     title="Go to Josh's profile"
-//     onPress={() => {
-//       navigation.navigate('Profile', {name: 'Josh'});
-//     }}
-//   />
-// );
-// return <Text>Count: {count}</Text>;
